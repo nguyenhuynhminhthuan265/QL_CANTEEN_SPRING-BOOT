a9067452dde2df2633febc938da4b108646f6a94
nguyenhuynhminhthuan265 <nha302031@gmail.com>
2019-12-12 14:55:24 +0700

    edit controller repository

--- QuanLy_Canteen/src/main/java/com/myclass/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myclass.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myclass.entity.Status;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-
-}
--- QuanLy_Canteen/src/main/java/com/myclass/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myclass.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myclass.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
